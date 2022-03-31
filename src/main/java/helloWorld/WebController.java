5540ff12744c24d625a1e2d1fd37ce8d90b11f9f
Brammy2904 <72151849+Brammy2904@users.noreply.github.com>
2022-03-31 18:02:09 +0200

    in principe versie 2 ;)

--- src/main/java/helloWorld/WebController.java
+++ src/main/java/helloWorld/WebController.java
@@ -1,7 +1,9 @@
 package helloWorld;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -17,38 +19,70 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class WebController {
-	@MessageMapping("/plane/1")
-	@SendTo("/plane/coords/1")
+	List<String> array = new ArrayList<String>();
+	String image = null;
+	@MessageMapping("/plane/move1")
+	@SendTo("/plane/coords")
 	public String getCoords(@RequestBody String coords){
 	    return coords;
 	   
 	}
 	@MessageMapping("/plane/shoot/1")
-	@SendTo("/plane/shoot1")
+	@SendTo("/plane/shoot")
 	public String shoot1(@RequestBody String test){
 	    return test;
 	   
 	}
 	@MessageMapping("/plane/boost/1")
-	@SendTo("/plane/boost1")
+	@SendTo("/plane/boost")
 	public String boost1(@RequestBody String test){
 	    return test;
 	   
 	}
-	@MessageMapping("/plane/2")
-	@SendTo("/plane/coords/2")
+	@MessageMapping("/plane/name")
+	@SendTo("/plane/name/test")
+	public Integer name(@RequestBody String test){
+		if(array.size() >= 2) {
+			array.clear();
+		}
+		array.add(test);
+	    return array.size();
+	   
+	}
+	@MessageMapping("/plane/move2")
+	@SendTo("/plane/coordsing")
 	public String getCoords2(@RequestBody String coords){
 	    return coords;
 	   
 	}
+	@MessageMapping("/plane/image")
+	@SendTo("/plane/image/settings")
+	public String getImage(@RequestBody String coords){
+		if(image == null) {
+			image = coords;
+			coords = "["+coords+"]";
+		}
+		else if(image != null) {
+			if((image.contains("player1") && coords.contains("player1")) ||(image.contains("player2") && coords.contains("player2")) ) {
+				coords = "["+coords+"]";
+				image = null;
+			}
+			else {
+			coords = "["+coords +","+ image+"]";
+			image = null;
+			}
+		}
+	    return coords;
+	   
+	}
 	@MessageMapping("/plane/shoot/2")
-	@SendTo("/plane/shoot2")
+	@SendTo("/plane/shooting")
 	public String shoot2(@RequestBody String test){
 	    return test;
 	   
 	}
 	@MessageMapping("/plane/boost/2")
-	@SendTo("/plane/boost2")
+	@SendTo("/plane/boosting")
 	public String boost2(@RequestBody String test){
 	    return test;
 	   
